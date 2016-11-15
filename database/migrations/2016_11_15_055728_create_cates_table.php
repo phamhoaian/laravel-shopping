bc18fdf16d1b284b59e5a9a1241c40a0b3434903
anph <anph@bit-vietnam.com>
2016-11-15 13:08:41 +0700

    create migrations

--- database/migrations/2016_11_15_055728_create_cates_table.php
+++ database/migrations/2016_11_15_055728_create_cates_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateCatesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,15 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('cates', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('username')->unique();
-			$table->string('password', 60);
-			$table->string('email');
-			$table->tinyInteger('level');
-			$table->rememberToken();
+			$table->string('name')->unique();
+			$table->string('alias');
+			$table->integer('order');
+			$table->integer('parent_id');
+			$table->string('keywords');
+			$table->string('description');
 			$table->timestamps();
 		});
 	}
@@ -31,7 +32,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('cates');
 	}
 
 }
--- database/migrations/2016_11_15_060639_create_product_images_table.php
+++ database/migrations/2016_11_15_060639_create_product_images_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateProductImagesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,12 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('product_images', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('username')->unique();
-			$table->string('password', 60);
-			$table->string('email');
-			$table->tinyInteger('level');
-			$table->rememberToken();
+			$table->string('image');
+			$table->integer('product_id')->unsigned();
+			$table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
 			$table->timestamps();
 		});
 	}
@@ -31,7 +29,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('product_images');
 	}
 
 }
